ef0c06a36fb39f0c6ba90f86ddd4eb41afd0dcc7
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-03-22 11:50:41 +0700

    Initial commit

---
+++ cache_redis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace cache_redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string platform = "64";
+
+            string file_node = Path.Combine(path, "x" + platform + @"\node.exe");
+
+            if (File.Exists(file_node))
+            {
+                Process p = new Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.FileName = file_node;
+                string argument = @" --max-old-space-size=4096 app.js";
+                p.StartInfo.Arguments = argument;
+                p.Start();
+                //p.WaitForExit();
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
