2976c52465d9b3cd9530c34a43cca5d939eb8471
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-04-04 06:21:16 +0700

    cachekv

---
+++ cachekv/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+
+namespace cachekv
+{ 
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/admin", (iab) =>
+            {
+                iab.Run(context =>
+                {
+                    context.Response.ContentType = "text/plain";
+                    return context.Response.WriteAsync("This is admin page");
+                });
+            });
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("My First Owin Application");
+            });
+        }
+
+    }
+
+    class Program
+    {
+        static Program()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (se, ev) =>
+            {
+                Assembly asm = null;
+                string comName = ev.Name.Split(',')[0];
+                string resourceName = @"DLL\" + comName + ".dll";
+                var assembly = Assembly.GetExecutingAssembly();
+                resourceName = typeof(Program).Namespace + "." + resourceName.Replace(" ", "_").Replace("\\", ".").Replace("/", ".");
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream != null)
+                    {
+                        byte[] buffer = new byte[stream.Length];
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            int read;
+                            while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                                ms.Write(buffer, 0, read);
+                            buffer = ms.ToArray();
+                        }
+                        asm = Assembly.Load(buffer);
+                    }
+                }
+                return asm;
+            };
+        }
+
+        public static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                var res = new HttpClient().GetAsync("http://localhost:12345/admin");
+                Console.WriteLine(res);
+                Console.WriteLine(res.Result.Content.ReadAsStringAsync().Result);
+
+                Console.WriteLine("Server Started...Press any key to exit");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
